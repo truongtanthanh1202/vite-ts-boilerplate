199ff0a97428341c1e58a658b3223d631a4f4426
anhtv56 <anhtv56@ghtk.co>
2022-04-12 17:16:18 +0700

    update

--- src/components/HelloWorld/index.vue
+++ src/components/HelloWorld/index.vue
@@ -16,20 +16,4 @@ const count = ref(0);
   </p>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
+<style scoped lang="scss" src="./styles.scss"></style>
