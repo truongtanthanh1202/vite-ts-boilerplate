bd03ef1f950c1ce21cd7713b19ff2ca105f03588
anhtv56 <anhtv56@ghtk.co>
2022-04-12 17:10:05 +0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'lodash/prefer-lodash-method': 'off',
+    'lodash/import-scope': 'off',
+    'lodash/prefer-noop': 'off',
+
+    'arrow-parens': 'off',
+    'import/order': 'off',
+    'comma-dangle': 'off',
+    'no-console': 'off',
+    'no-debugger': 'warn',
+    'no-var': 'error',
+    'no-unused-vars': 'off',
+    'block-scoped-var': 'error',
+    'no-param-reassign': 'error',
+    semi: ['warn', 'always'],
+    'space-before-function-paren': ['off', 'never'],
+    'key-spacing': [
+      'warn',
+      {
+        mode: 'minimum',
+        beforeColon: false,
+        afterColon: true,
+      },
+    ],
+    'arrow-spacing': ['warn'],
+    'spaced-comment': ['warn', 'always'],
+    'space-in-parens': ['warn', 'never'],
+    'object-curly-spacing': ['warn', 'always'],
+    'no-trailing-spaces': 'warn',
+    'object-shorthand': ['warn', 'methods'],
+    'prefer-template': 'warn',
+    'no-useless-concat': 'warn',
+    'prefer-const': 'error',
+    yoda: ['warn', 'never', { exceptRange: true }],
+    // "indent": ["warn", 2, { "SwitchCase": 1 }],
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    'max-len': [
+      'warn',
+      110,
+      {
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignorePattern: 'd="([\\s\\S]*?)"',
+      },
+    ],
+    'no-constant-condition': [
+      'warn',
+      {
+        checkLoops: false,
+      },
+    ],
+    'brace-style': ['warn'],
+    eqeqeq: ['warn', 'always'],
+    camelcase: 'off',
+
+    // typescript
+    '@typescript-eslint/no-unused-vars': ['warn', { vars: 'all', args: 'none' }],
+
+    // vue
+    'vue/attribute-hyphenation': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+};
