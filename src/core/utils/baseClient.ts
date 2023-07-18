14c78f67b9b066f630bf8b20c8224854bf9f884f
anhtv56 <anhtv56@ghtk.co>
2023-07-18 11:01:16 +0700

    feat: update

---
+++ src/core/utils/baseClient.ts
@@ -0,0 +1,156 @@
+import axios, { AxiosError, AxiosResponse, AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+
+import { IResponse } from '../interfaces';
+import { authService } from '@/services';
+
+interface ConfigInstance {
+  setAuthorizationFn?: (config: InternalAxiosRequestConfig) => InternalAxiosRequestConfig;
+}
+
+export class BaseClient {
+  private isRefreshing = false;
+  private failedQueue: any[] = [];
+  baseURL: string = '';
+  headers: any = {};
+
+  constructor({ baseURL, headers }: { baseURL: string; headers?: any }) {
+    this.baseURL = baseURL;
+    this.headers = headers;
+  }
+
+  private processQueue(error: AxiosError | null, token = null) {
+    this.failedQueue.forEach((item) => {
+      if (error) {
+        item.reject(error);
+      } else {
+        item.resolve(token);
+      }
+    });
+
+    this.failedQueue = [];
+  }
+
+  private rejectErrorAndClearToken(error: AxiosError) {
+    return Promise.reject(error);
+  }
+
+  private transformResponse(res: AxiosResponse): IResponse {
+    const resData = res.data || {};
+    const success = !!resData.success;
+    return {
+      success,
+      error: !success,
+      data: resData.data,
+      statusCode: res?.status,
+      message: resData.message,
+      rawResponse: res,
+    };
+  }
+
+  private transformError(error: AxiosError<any, any>): IResponse {
+    const res = error.response;
+    const resData = res?.data || {};
+    return {
+      success: false,
+      error: true,
+      data: resData.data,
+      statusCode: res?.status || error.code,
+      message: resData.message || error.message,
+      rawResponse: res,
+    };
+  }
+
+  create({ setAuthorizationFn }: ConfigInstance) {
+    const defaultSetAuthorizationFn = (config) => {
+      const token: string = 'token';
+
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+    };
+
+    const api: AxiosInstance = axios.create({
+      baseURL: this.baseURL,
+      timeout: 30000,
+      headers: {
+        Accept: 'application/json',
+        ...this.headers,
+      },
+    });
+
+    api.interceptors.request.use((config) => {
+      if (!config.headers) {
+        return config;
+      }
+
+      if (setAuthorizationFn) {
+        return setAuthorizationFn(config);
+      }
+
+      defaultSetAuthorizationFn(config);
+
+      return config;
+    });
+
+    api.interceptors.response.use(
+      (response: AxiosResponse): any => {
+        return this.transformResponse(response);
+      },
+      async (error: AxiosError) => {
+        const originalRequest: any = error.config;
+
+        // Only handle when status == 401
+        if (error?.response?.status !== 401) {
+          return this.transformError(error);
+        }
+
+        // Clear token and throw error when retried
+        if (originalRequest._retry) {
+          return this.rejectErrorAndClearToken(error);
+        }
+
+        // If refresh token is not valid and server response status == 401
+        if (originalRequest.url === 'auth/refresh-token') {
+          return this.rejectErrorAndClearToken(error);
+        }
+
+        // Handle if token is refreshing
+        if (this.isRefreshing) {
+          return new Promise((resolve, reject) => {
+            this.failedQueue.push({ resolve, reject });
+          })
+            .then((token) => {
+              return api(originalRequest);
+            })
+            .catch((err) => {
+              return Promise.reject(err);
+            });
+        }
+
+        // Set variables
+        originalRequest._retry = true;
+        this.isRefreshing = true;
+
+        // Call request refresh token
+        const res = await authService
+          .refreshToken()
+          .catch((err: AxiosError) => {
+            this.processQueue(err);
+            return this.rejectErrorAndClearToken(err);
+          })
+          .finally(() => {
+            this.isRefreshing = false;
+          });
+
+        if (res && res?.success) {
+          this.processQueue(null, res.payload.access_token);
+          return Promise.resolve(api(originalRequest));
+        }
+
+        return this.rejectErrorAndClearToken(error);
+      },
+    );
+
+    return api;
+  }
+}
--- src/core/utils/httpClient.ts
+++ src/core/utils/httpClient.ts
@@ -1,131 +1,17 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
-import { IResponse } from '../interfaces';
-import { authService } from '@/services';
+import { BaseClient } from './baseClient';
 
-let isRefreshing = false;
-let failedQueue: any[] = [];
-
-const processQueue = (error: AxiosError | null, token = null) => {
-  failedQueue.forEach((item) => {
-    if (error) {
-      item.reject(error);
-    } else {
-      item.resolve(token);
-    }
-  });
-
-  failedQueue = [];
-};
-
-function rejectErrorAndClearToken(error: AxiosError) {
-  return Promise.reject(error);
-}
-
-function transformRespose(res: AxiosResponse): IResponse {
-  const resData = res.data || {};
-  const success = !!resData.success;
-  return {
-    success,
-    error: !success,
-    data: resData.data,
-    statusCode: res?.status,
-    message: resData.message,
-    rawResponse: res,
-  };
-}
-
-function tranformError(error: AxiosError<any, any>): IResponse {
-  const res = error.response;
-  const resData = res?.data || {};
-  return {
-    success: false,
-    error: true,
-    data: resData.data,
-    statusCode: res?.status || error.code,
-    message: resData.message || error.message,
-    rawResponse: res,
-  };
-}
-
-const api: AxiosInstance = axios.create({
+export const httpClient: AxiosInstance = new BaseClient({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 30000,
-  headers: {
-    Accept: 'application/json',
-  },
-});
+}).create({
+  setAuthorizationFn: (config: InternalAxiosRequestConfig) => {
+    const token: string = 'token';
 
-api.interceptors.request.use((config) => {
-  const accessToken = '';
-  if (!config.headers) {
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
     return config;
-  }
-
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-
-  return config;
+  },
 });
-
-api.interceptors.response.use(
-  (response: AxiosResponse): IResponse => {
-    return transformRespose(response);
-  },
-  async (error: AxiosError) => {
-    const originalRequest: any = error.config;
-
-    // Only handle when status == 401
-    if (error?.response?.status !== 401) {
-      return tranformError(error);
-    }
-
-    // Clear token and throw error when retried
-    if (originalRequest._retry) {
-      return rejectErrorAndClearToken(error);
-    }
-
-    // If refresh token is not valid and server response status == 401
-    if (originalRequest.url === 'auth/refresh-token') {
-      return rejectErrorAndClearToken(error);
-    }
-
-    // Handle if token is refreshing
-    if (isRefreshing) {
-      return new Promise((resolve, reject) => {
-        failedQueue.push({ resolve, reject });
-      })
-        .then((token) => {
-          return api(originalRequest);
-        })
-        .catch((err) => {
-          return Promise.reject(err);
-        });
-    }
-
-    // Set variables
-    originalRequest._retry = true;
-    isRefreshing = true;
-
-    // Call request refresh token
-    const res = await authService
-      .refreshToken()
-      .catch((err: AxiosError) => {
-        processQueue(err);
-        return rejectErrorAndClearToken(err);
-      })
-      .finally(() => {
-        isRefreshing = false;
-      });
-
-    if (res && res?.success) {
-      processQueue(null, res.payload.access_token);
-      return Promise.resolve(api(originalRequest));
-    }
-
-    return rejectErrorAndClearToken(error);
-  },
-);
-
-export default api;
