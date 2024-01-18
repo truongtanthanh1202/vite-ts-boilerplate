58749c25091cb770b96f85124010c9fef0255cbb
anhtv56 <anhtv56@ghtk.co>
2024-01-18 10:09:58 +0700

    feat: update date lib

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import DatePicker from 'ant-design-vue/es/date-picker/date-fns';
+import TimePicker from 'ant-design-vue/es/time-picker/date-fns';
+import Calendar from 'ant-design-vue/es/calendar/date-fns';
 
 import router from '@/router';
 import App from './App.vue';
@@ -10,4 +13,5 @@ import './assets/styles/app.scss';
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
+app.use(DatePicker).use(TimePicker).use(Calendar);
 app.mount('#__app');
