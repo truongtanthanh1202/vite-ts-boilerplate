33d6bea07f1d5c4f881f2cda4d7a4b766d35812f
anhtv56 <anhtv56@ghtk.co>
2025-07-25 15:24:25 +0700

    update

--- src/shared/constants/theme.ts
+++ src/shared/constants/theme.ts
@@ -1,8 +1,7 @@
-import style from '@/assets/styles/common/variable.module.scss';
-
 export const theme = {
   token: {
-    fontFamily: style.fontFamily,
+    fontFamily:
+      "-apple-system, roboto, blinkmacsystemfont, 'Segoe UI', 'Helvetica Neue', arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
     colorPrimary: '#069255',
     fontSize: 16,
     borderRadius: 4,
