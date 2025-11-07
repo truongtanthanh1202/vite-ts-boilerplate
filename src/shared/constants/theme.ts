257f9c86bc54f1807ffad71c105cb02cbbf56094
anhtv56 <anhtv56@ghtk.co>
2025-11-07 10:20:21 +0700

    update

--- src/shared/constants/theme.ts
+++ src/shared/constants/theme.ts
@@ -1,6 +1,8 @@
 import viVN from 'ant-design-vue/es/locale/vi_VN';
+import { Locale } from 'ant-design-vue/es/locale';
+import { ThemeConfig } from 'ant-design-vue/es/config-provider/context';
 
-export const theme = {
+export const theme: ThemeConfig = {
   token: {
     fontFamily: 'var(--font-family-base)',
     colorPrimary: '#069255',
@@ -9,7 +11,7 @@ export const theme = {
   },
 };
 
-export const ANT_LOCALE = {
+export const ANT_LOCALE: Locale = {
   ...viVN,
   Empty: {
     ...viVN.Empty,
